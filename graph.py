5f42fd4e9dba07bb03ac13dee219270433d3bf35
matizimny8 <mzimniewicz1@gmail.com>
2026-05-19 20:29:45 +0200

    nonhamilton chyba dzialajacy xd

--- graph.py
+++ graph.py
@@ -5,6 +5,8 @@ class Graph:
     self.edges = []
     self.saturation = saturation
     self.current_edges_count = 0
+    self.edges_to_create = 0
+    
   def add_edge(self, a, b):
     self.edges.append((min(a,b),max(a,b)))
     self.current_edges_count+=1
@@ -13,11 +15,11 @@ class Graph:
     nodes_list = [i for i in range (1,self.nodes+1)]
     random.shuffle(nodes_list)
     max_edges = (self.nodes * (self.nodes - 1))//2
-    edges_to_create = int((self.saturation / 100.0) * max_edges)
+    self.edges_to_create = int((self.saturation / 100.0) * max_edges)
     for i in range(len(nodes_list)-1):
       self.add_edge(nodes_list[i],nodes_list[i+1])
     self.add_edge(nodes_list[-1],nodes_list[0])
-    while self.current_edges_count+3 <= edges_to_create:
+    while self.current_edges_count+3 <= self.edges_to_create:
       selected_nodes = random.sample(nodes_list,3)
       edges_list = self.check_if_edges_exist(selected_nodes)
       if edges_list != 0:
@@ -43,14 +45,20 @@ class Graph:
     nodeToDel = self.nodes
     nowe_krawedzie = []
     deleted_count = 0
+    nodes_list = [i for i in range (1,self.nodes+1)]
     for edge in self.edges:
         if edge[0] == nodeToDel or edge[1] == nodeToDel:
-            licznik_usunietych += 1
+            deleted_count += 1
         else:
             nowe_krawedzie.append(edge)
     self.edges = nowe_krawedzie
-    self.current_edges_count -= licznik_usunietych
-
+    self.current_edges_count -= deleted_count
+    while self.current_edges_count <= self.edges_to_create:
+      selected_nodes = random.sample(nodes_list,2)
+      selected_nodes.sort()
+      if (selected_nodes) not in self.edges:
+        self.add_edge(selected_nodes[0],selected_nodes[1])
+    
   def print_graph(self):
     for start, destination in self.edges:
       print(f"{start} -> {destination}")
